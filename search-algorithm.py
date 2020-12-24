5ac45a870b57f45b32870ee5ace878999550850e
yassataiseer <1taiseeryas@hdsb.ca>
2020-12-24 16:17:46 -0500

    improved algorith

--- search-algorithm.py
+++ search-algorithm.py
@@ -1,32 +1,46 @@
 from difflib import SequenceMatcher
 
 
-hashtags = {"pic_one":["#cat","#water"]}
+hashtags = {"pic_one":["#cat","#cute"] ,"pic_two":["#baby","#cute"],"pic_three":["#laptop"]}
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 def get_key(val):
+    answer = []
     for key, value in hashtags.items():
         for components in value:
             if similar(val,components)>=0.5:
-                return key
+                if key in answer:
+                    pass
+                else:
+                    answer.append(key)
             else:
-                return "no"
-                
+                pass
+    return answer                
             
-
+ 
 
 #print(similar("curcatsast","cat")) 
 
-#for i in hashtags:
-#    for x in hashtags[i]:
-#        x = str(x)
-#        print(get_key('#cat'))
+for i in hashtags:
+    for x in hashtags[i]:
+        x = str(x)
+        print(get_key('#cute'))
 
-query("")
+
+
+
+
+'''
+query = []
 a = "#hastag#youtube#funguy"
 a = a.split("#")
 for i in a:
-    i = "#"+i
-print(a)
+    if len(i) == 0:
+        pass
+    else:
+        i = "#"+i
+        query.append(i)
+    
+print(query)'''
 
