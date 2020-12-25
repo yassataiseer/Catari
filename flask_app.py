ae7ce078c820c65532851274686b580363450edc
yassataiseer <1taiseeryas@hdsb.ca>
2020-12-24 19:09:27 -0500

    commit on main

--- flask_app.py
+++ flask_app.py
@@ -14,6 +14,13 @@ def index():
 def login_link():
     return render_template("login.html")
 
+
+@app.route("/search")
+def search():
+    return render_template("search_engine.html")
+
+
+
 @app.route("/signup-link")
 def signup_link():
     return render_template("sign-up.html")
@@ -25,7 +32,7 @@ def login_user():
     query = users.Validate_user(email,password)
     print(query)
     if query== True:
-        return "logged in"
+        return render_template("search-engine.html")
     else:
         return "invalid creds." 
 
--- search-algorithm.py
+++ search-algorithm.py
@@ -5,42 +5,42 @@ import sqlite3
 conn = sqlite3.connect('images.db')
 c = conn.cursor()
 
-
-def dictionary():
-    hastags = {}
-    values_images = c.execute("SELECT * FROM stuffToPlot")
-    data = c.fetchall()
-    for i in data:
-        a = i[1].split("#")
-        del a[0]
-        for x in a:
-            if len(x)==0:
-                pass
-            else:
-                x = "#"+x
-                hastags[i[0]] = a
-        #print(i[0])
-        #print(i[1])
-    return hastags
-
-
-def similar(a, b):
-    return SequenceMatcher(None, a, b).ratio()
-
-def get_key(val):
-    hashtags = dictionary()
-    answer = []
-    for key, value in hashtags.items():
-        for components in value:
-            if similar(val,components)>=0.5:
-                if key in answer:
+class search:
+    def dictionary():
+        hastags = {}
+        values_images = c.execute("SELECT * FROM stuffToPlot")
+        data = c.fetchall()
+        for i in data:
+            a = i[1].split("#")
+            del a[0]
+            for x in a:
+                if len(x)==0:
                     pass
                 else:
-                    answer.append(key)
-            else:
-                pass
-    return answer                
+                    x = "#"+x
+                    hastags[i[0]] = a
+            #print(i[0])
+            #print(i[1])
+        return hastags
+
+
+    def similar(a, b):
+        return SequenceMatcher(None, a, b).ratio()
+
+    def get_key(val):
+        hashtags = search.dictionary()
+        answer = []
+        for key, value in hashtags.items():
+            for components in value:
+                if search.similar(val,components)>=0.5:
+                    if key in answer:
+                        pass
+                    else:
+                        answer.append(key)
+                else:
+                    pass
+        return answer                
             
-#print(get_key("baby"))
+#print(search.get_key("cute cat"))
 
 
